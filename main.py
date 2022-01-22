b3c9e804fd751526f628a792d0242d0e52c6b4a2
shubhp2610 <shubhpatel2610@outlook.com>
2022-01-22 12:08:50 +0530

    Create main.py
    Block and proof-of-work added

---
+++ main.py
@@ -0,0 +1,39 @@
+# Importing libraries
+from dataclasses import dataclass
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part - 1 Building blockchain
+
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, previous_hash='0')
+    # right after block is mined
+
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain)+1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(
+                str(new_proof**2 - previous_proof**2).encode())  # non symmetric
+            if hash_operation[:4] == '0000':
+                check_proof=True
+            else:
+                new_proof += 1
+        return new_proof    
